233b0dde343669af764a33d3932493e0c8a4b11c
saadqureshi00714 <saad_habib@live.com>
2025-04-15 16:56:37 +0800

    Update changeover_app.py

--- changeover_app.py
+++ changeover_app.py
@@ -3,6 +3,26 @@ import streamlit as st
 import plotly.express as px
 import os
 
+# ---------- STREAMLIT PASSWORD PROTECTION ----------
+
+def check_password():
+    def password_entered():
+        if st.session_state["password"] == "ottimo123":
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        st.text_input("Enter Password", type="password", on_change=password_entered, key="password")
+        st.stop()
+    elif not st.session_state["password_correct"]:
+        st.text_input("Enter Password", type="password", on_change=password_entered, key="password")
+        st.error("😕 Incorrect password")
+        st.stop()
+
+check_password()
+
 # ---------- DATA PROCESSING SECTION ----------
 
 # Load the original Excel file
